d904bd97266d6d1dab1ba80c9c2f0ecad2c60052
uanderson21 <uanderson21@yahoo.com.br>
2023-07-15 09:35:15 -0300

    Correção no GameService implementando anotação @Service e configuração banco de dados H2 para teste.

--- src/main/java/br/com/uss/dlist/service/GameService.java
+++ src/main/java/br/com/uss/dlist/service/GameService.java
@@ -3,6 +3,7 @@ package br.com.uss.dlist.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.uss.dlist.dto.GameDTO;
@@ -10,6 +11,7 @@ import br.com.uss.dlist.dto.GameMinDTO;
 import br.com.uss.dlist.entities.Game;
 import br.com.uss.dlist.repositories.GameRepository;
 
+@Service
 public class GameService {
 
 	@Autowired
