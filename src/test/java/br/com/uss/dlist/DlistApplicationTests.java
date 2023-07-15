885930cc1c745a68c2d265948796c94fa296bbf9
uanderson21 <uanderson21@yahoo.com.br>
2023-07-15 09:04:53 -0300

    Criando a base do projeto Lista de Games com as dependencias

---
+++ src/test/java/br/com/uss/dlist/DlistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uss.dlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
